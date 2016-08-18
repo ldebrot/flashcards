f1c4ede1947e82eafa8655731933ad4cd57e33eb
ldebrot <ldebrot@yahoo.de>
2016-08-18 12:31:59 +0200

    Improved detection of start- and endelements, added Inferential Statistics, added Reproducible Research

--- studyblue_csv_to_rda.R
+++ studyblue_csv_to_rda.R
@@ -24,7 +24,7 @@ cat("\n Working directory set to the one in which this script is located \n")
 
 # Set error messages for further use
 errmsg1 <- "ERROR 1 - This is not a correct choice - choose a number corresponding to a file"
-errmsg2 <- "ERROR 2 - Something went wrong with the identification of the starting and ending elements. Their number is not equal. Sorry, I failed you... :("
+errmsg2 <- "ERROR 2 - Something went wrong with the identification of the starting and ending elements, despite 100 sincere attempts. Sorry, I failed you... :("
 
 # File choice
 cat("\n *** \n \n Available CSV files in your working directory : \n")
@@ -43,14 +43,70 @@ fnumber <- x
 fcname <- dir(pattern=filepattern)[fnumber]
 flines <- readLines(con=fcname)
 
-# Define starting and ending patterns
-fpat_start <- '^"' # starts with a quotation mark
-fpat_end <- '"$' # ends with a quotation mark
+# Check whether first line starts with a quotation mark (as Studyblue provides two types of csv files...)
+tmp_1stchar <- substr(flines[1],1,1)
+
+# Apply restrictive patterns
+fpat_start <- '[,]{1}["]{1}' # starts with a quotation mark (old pattern : '^"')
+fpat_end <- '[^"]["]{1}$' # ends with a quotation mark
+
+    fpat_start <- '^"' # starts with a quotation mark
+    fpat_end <- '"$' # ends with a quotation mark
 
 # Identify starting and ending elements in the read elements
 startelements <- grep(pattern=fpat_start,x=flines)
 endelements <- grep(pattern=fpat_end,x=flines)
-if (length(startelements)!=length(endelements)) stop(errmsg2) # Just checking whether we identified an equal number of starting and ending elements
+
+
+# Check whether equal numbers of start- and endelements
+safetycount <- 0
+checkreport <- "" #REPORT
+while (length(startelements)!=length(endelements)) {
+    safetycount <- safetycount + 1
+    if (safetycount>100) stop(errmsg2)
+    tmp_counts <- max(c(length(startelements),length(endelements)))
+    for (i in 1:tmp_counts) {
+        if(endelements[i]<startelements[i]) { #If endelement is below or equal to the next starting element, there is a missing endelement
+            newreport <- paste("\n \n Unequal number of start- and endelements at element #",i," which is startelement #",startelements[i]," and endelement #",endelements[i]," \n \n ",collapse="")
+            checkreport <- c(checkreport,newreport)
+
+            # Check whether the previous endelement was right
+            tmp_pos <- endelements[i-1] #Get the position of the last endelement
+            tmp_nxt <- flines[tmp_pos+1] #Get the element after the position of the last endelement
+
+            # Check whether this looks like a start element
+            tmp_eval <- 0
+            if(length(grep(pattern='[,]{1}["]{1}',x=tmp_nxt))==1) tmp_eval <- tmp_eval + 1
+            if(length(grep(pattern='^"[A-Z]',x=tmp_nxt))==1) tmp_eval <- tmp_eval + 1
+
+            if(tmp_eval!=2) { #the next element is NOT a starting element, so the endelement is wrong !
+                newreport <- paste("Endelement wrong, next element is NOT a starting element. Endelement deleted.")
+                checkreport <- c(checkreport,newreport)
+                oldendelements <- endelements
+                endelements <- NULL
+                endelements[1:(i-2)] <- oldendelements [1:(i-2)]
+                endelements[(i-1):(length(oldendelements)-1)] <- oldendelements[i:length(oldendelements)]
+            } 
+
+            if(tmp_eval==2) { #the next element IS INDEED a starting element, so the startelement is wrong
+                newreport <- paste("Startelement wrong, element following last endelement IS a starting element. Startelement added.")
+                checkreport <- c(checkreport,newreport)
+                oldstartelements <- startelements
+                startelements <- NULL
+                startelements[1:(i-1)] <- oldstartelements [1:(i-1)]
+                startelements[i] <- tmp_pos+1
+                startelements[(i+1):(length(oldstartelements)+1)] <- oldstartelements[i:length(oldstartelements)]
+            }
+            
+            break
+        }
+    }
+}
+
+#REPORT
+newreport <- "\n \n Conversion all fine."
+checkreport <- c(checkreport,newreport)
+
 
 elementslength <- length(startelements)
 
@@ -84,4 +140,5 @@ fcards$number <- as.numeric(as.character(fcards$number))
 ## Save it as a RDA file
 fcrdaname <- sub(pattern=".csv",replacement=".Rda",x=fcname)
 save(fcards,file=fcrdaname)
+cat(checkreport)
 cat("\n \n *** \n Saved a RDA file containing the flashcard content in your working directory \n \n ")
